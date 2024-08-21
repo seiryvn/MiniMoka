c2d92f93178e41845ddc0ad4501dedeae984073b
Fiona Ni <FionaNi71@gmail.com>
2024-08-21 14:22:33 -0400

    finished implementing link cards

--- src/app/my-components/ContentCard.js
+++ src/app/my-components/ContentCard.js
@@ -16,18 +16,16 @@ export function ContentCard({logged1, logged2, logged3, logged4}) {
         <div className="flex flex-row items-center space-x-4 z-10">
           <div className="flex flex-col">
             <p className="font-normal text-base text-stone-500 relative z-10">
-                sdasd
             </p>
-            <p className="text-sm text-s-400">asdasd</p>
+            
           </div>
         </div>
         <div className="text content">
           <h1 className="font-bold text-xl md:text-2xl text-black-700 relative z-10">
-            Author Card
+            Nanci
           </h1>
           <p className="font-normal text-sm text-black-500 relative z-10 my-4">
-            Card with Author avatar, complete name and time to read - most
-            suitable for blogs.
+          Mixing traditional artistry, beauty is captured with Nanci.
           </p>
         </div>
       </div>
@@ -42,14 +40,19 @@ export function ContentCard({logged1, logged2, logged3, logged4}) {
         }
       >
         <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-white opacity-20"></div>
-        
+        <div className="flex flex-row items-center space-x-4 z-10">
+          <div className="flex flex-col">
+            <p className="font-normal text-base text-stone-500 relative z-10">
+            </p>
+            
+          </div>
+        </div>
         <div className="text content">
           <h1 className="font-bold text-xl md:text-2xl text-black-700 relative z-10">
-            Author Card
+            Azura
           </h1>
           <p className="font-normal text-sm text-black-500 relative z-10 my-4">
-            Card with Author avatar, complete name and time to read - most
-            suitable for blogs.
+          Elevate your display with the enchanting Azura series.
           </p>
         </div>
       </div>
@@ -67,18 +70,16 @@ export function ContentCard({logged1, logged2, logged3, logged4}) {
         <div className="flex flex-row items-center space-x-4 z-10">
           <div className="flex flex-col">
             <p className="font-normal text-base text-stone-50 relative z-10">
-              Manu Arora
+            
             </p>
-            <p className="text-sm text-gray-400">2 min read</p>
           </div>
         </div>
         <div className="text content">
-          <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
-            Author Card
+          <h1 className="font-bold text-xl md:text-2xl text-black-700 relative z-10">
+            Hirono
           </h1>
-          <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
-            Card with Author avatar, complete name and time to read - most
-            suitable for blogs.
+          <p className="font-normal text-sm text-black-500 relative z-10 my-4">
+          Hirono figurines: Where artistry meets charm.
           </p>
         </div>
       </div>
@@ -96,16 +97,14 @@ export function ContentCard({logged1, logged2, logged3, logged4}) {
         <div className="flex flex-row items-center space-x-4 z-10">
           <div className="flex flex-col">
             <p className="font-normal text-base text-gray-50 relative z-10">
-              Manu Arora
             </p>
-            <p className="text-sm text-gray-400">2 min read</p>
           </div>
         </div>
         <div className="text content">
-          <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
-            Author Card
+          <h1 className="font-bold text-xl md:text-2xl text-black-700 relative z-10">
+            Skull Panda
           </h1>
-          <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
+          <p className="font-normal text-sm text-black-500 relative z-10 my-4">
             Card with Author avatar, complete name and time to read - most
             suitable for blogs.
           </p>
